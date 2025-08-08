dab275a9abc8b985312105d531e6787958adebdc
falcon9205 <mauryahimanshu567@gmail.com>
2025-08-08 13:32:43 +0530

    update admin credentials

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const DB_URI = "mongodb+srv://mauryahimanshu567:mWkUon0OxW6kPnDD@msend-cluster.ar3gh.mongodb.net/?retryWrites=true&w=majority&appName=msend-cluster"
 const JWT_Key = "test"
-const AdminEmail = "t@gmail.com"
-const AdminPassword = "123456"
+const AdminEmail = "admin@codesfortomorrow.com"
+const AdminPassword = "Admin123!@#"
 
 
 module.exports = {DB_URI,JWT_Key,AdminEmail,AdminPassword}
